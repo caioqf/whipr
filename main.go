2a85292aa03fe8ba37ae57d4c1758a14bec704ff
caioqf <trickshot2015@proton.me>
2025-07-24 23:34:53 -0300

    refactor(main): reaplace execution logic with cobra

--- main.go
+++ main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
+	// "fmt"
+	// "net"
+	// "os"
+	// "github.com/getlantern/systray"
 
-	"github.com/getlantern/systray"
+	"github.com/caioqf/whipr/cmd"
 )
 
 const sockPath = "/tmp/whipr.sock"
 
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "--shortcut" {
-		conn, err := net.Dial("unix", sockPath)
-		if err != nil {
-			fmt.Println("whipr not running.")
-			return
-		}
-		conn.Write([]byte("translate"))
-		conn.Close()
-		return
-	}
-	systray.Run(OnReady, OnExit)
+	// if len(os.Args) > 1 && os.Args[1] == "--shortcut" {
+	// 	conn, err := net.Dial("unix", sockPath)
+	// 	if err != nil {
+	// 		fmt.Println("whipr not running.")
+	// 		return
+	// 	}
+	// 	conn.Write([]byte("translate"))
+	// 	conn.Close()
+	// 	return
+	// }
+	// systray.Run(OnReady, OnExit)
+
+	cmd.Execute()
+
 }
